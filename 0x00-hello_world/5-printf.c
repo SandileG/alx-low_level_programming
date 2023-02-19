327519fb747b6c44ad6cc1d258232e55f8fe6bdb
SandileG <sandilegraham@gmail.com>
2023-02-19 08:46:18 -0800

    5-printf.c - attempted

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the provided string using printf not puts
+ * Return: 0 (Desired Result)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
