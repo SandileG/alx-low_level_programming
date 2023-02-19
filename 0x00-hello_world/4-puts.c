9dc50333d63609147203effc26a7a2b14354fd5d
SandileG <sandilegraham@gmail.com>
2023-02-19 05:36:36 -0800

    4-puts.c - attempted

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses the puts function.
+ * Return: 0 (Desired output)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
