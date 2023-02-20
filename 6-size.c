a2914baa3595d81fd23da26dc3bc189d9380d1e1
SandileG <sandilegraham@gmail.com>
2023-02-20 04:55:08 -0800

    6-size.c - I'm about to pull my hair out, and I don;t have hair!

---
+++ 6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Must create a program that prints sizes of various data types
+* Return: 0 (Desired Result)
+*/
+int main(void)
+{
+	char age;
+	int of;
+	long int ultron;
+	long long int draws;
+	float nearer;
+
+printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof(age));
+printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof(of));
+printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof(ultron));
+printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof(draws));
+printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof(nearer));
+return (0);
+}
