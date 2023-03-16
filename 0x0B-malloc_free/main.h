deedbd16f27f27f5fc8c57fb0df25757b231e3bd
SandileG <sandilegraham@gmail.com>
2023-03-15 17:40:10 -0700

    Main header file added

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
-
 char *create_array(unsigned int size, char c);
 
 char *_strdup(char *str);
@@ -16,5 +14,7 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 
 char **strtow(char *str);
+
+int _putchar(char c);
 
 #endif
