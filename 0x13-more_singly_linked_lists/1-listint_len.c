6bbf8e710b0c0aa98c703d066197c3e36343b57f
SandileG <sandilegraham@gmail.com>
2023-04-04 07:17:38 -0700

    Betty fixed

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,14 +11,14 @@ size_t listint_len(const listint_t *h)
 
 {
 
- size_t val = 0;
+	size_t val = 0;
 
- while (h)
- {
- val++;
- h = h->next;
- }
+	while (h)
+	{
+		val++;
+		h = h->next;
+	}
 
- return (val);
+	return (val);
 
 }
