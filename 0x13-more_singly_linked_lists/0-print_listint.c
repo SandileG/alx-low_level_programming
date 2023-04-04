4b421654e997b8613cb06b97e16cc740466b81ed
SandileG <sandilegraham@gmail.com>
2023-04-04 16:52:19 -0700

    0-print_listint.c - fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,15 @@ size_t print_listint(const listint_t *h)
 
 {
 
-size_t num = 0;
+int val = 0;
 
 while (h)
 
 {
 	printf("%d\n", h->n);
-	num++;
+	val++;
 	h = h->next;
 }
 
-return (num);
+return (val);
 }
