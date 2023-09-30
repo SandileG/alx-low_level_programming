a12a04ef96995018d9bc9acd3d51e375455b1517
SandileG <sandilegraham@gmail.com>
2023-09-30 16:52:14 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,16 +10,16 @@
 
 int _sqrt_recursion(int n)
 {
-    /* Base case: If n is negative, return -1. */
-    if (n < 0)
-        return -1;
+	/* Base case: If n is negative, return -1. */
+	if (n < 0)
+	return (-1);
 
-    /* Base case: The square root of 0 or 1 is the number itself. */
-    if (n == 0 || n == 1)
-        return n;
+	/* Base case: The square root of 0 or 1 is the number itself. */
+	if (n == 0 || n == 1)
+	return (n);
 
-    /* Recursive case: Try the next guess. */
-    return sqrt_helper(n, 1);
+	/* Recursive case: Try the next guess. */
+	return (sqrt_helper(n, 1));
 }
 
 /**
@@ -32,15 +32,14 @@ int _sqrt_recursion(int n)
 
 int sqrt_helper(int n, int guess)
 {
-    /* If guess^2 is n, we found the square root. */
-    if (guess * guess == n)
-        return guess;
+	/* If guess^2 is n, we found the square root. */
+	if (guess * guess == n)
+	return (guess);
 
-    /* If guess^2 is greater than n, n doesn't have a natural square root. */
-    if (guess * guess > n)
-        return -1;
+	/* If guess^2 is greater than n, n doesn't have a natural square root. */
+	if (guess * guess > n)
+	return (-1);
 
-    /* Increment guess and continue searching recursively. */
-    return sqrt_helper(n, guess + 1);
+	/* Increment guess and continue searching recursively. */
+	return (sqrt_helper(n, guess + 1));
 }
-
