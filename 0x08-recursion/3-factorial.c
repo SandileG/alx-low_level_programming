ccffbcdd6a9999e051b049373d9e642bc0354586
SandileG <sandilegraham@gmail.com>
2023-09-30 07:18:33 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,11 +11,11 @@ int factorial(int n)
 {
 	/* Base case: if n is negative, return -1 to indicate error. */
 	if (n < 0)
-		(return -1);
+		return (-1);
 
 	/* Base case: if n is 0, factorial is 1  */
 	if (n == 0)
-		(return 1);
+		return (1);
 
 	/* Recursive case: multply n with factorial of (n-1) */
 	return (n * factorial(n - 1));
