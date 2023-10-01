bf2a7a10b5c95052396ad03d89c2abbe75316de5
SandileG <sandilegraham@gmail.com>
2023-10-01 14:21:52 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Check if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	/* 0 and 1 are not prime numbers. */
+	if (n <= 1)
+	return 0;
+
+	/* Check for divisibility by numbers from 2 to the square root of n */
+	for (int i = 2; i * i <= n; i++)
+	{
+	if (n % i == 0)
+	return 0; /* Not prime, as it's divisible by i */
+	}
+
+	return 1; /* If no divisors found, it's prime */
+}
