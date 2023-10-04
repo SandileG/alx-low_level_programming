518f8095e1ae584f58519cc5a543edfb517e2f8d
SandileG <sandilegraham@gmail.com>
2023-10-04 04:00:07 -0700

    All work completed.

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings, only upto n characters from src.
+ * _strncat - Concatenates two strings, up to n characters from src.
  * @dest: Pointer to the destination string.
  * @src: Pointer to the source string to be appended.
  * @n: Maximum number of characters to append from src.
  *
  * Return: Pointer to concatenated string (dest).
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len = 0;
@@ -21,7 +20,14 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	/* Append up to n characters from src to dest. */
-	dest[dest_len + i] = '\0';
+	while (src[i] != '\0' && i < n)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	dest[dest_len + i] = '\0'; /* Null-terminate the concatenated string */
 
 	return (dest);
 }
+
