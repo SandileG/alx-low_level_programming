88616ff0772a74b9c7cd157aae6f2014e50fdb75
SandileG <sandilegraham@gmail.com>
2023-10-07 11:17:19 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,11 +9,11 @@
  * Return: Always 0 (Success).
  */
 
- int main(int argc, char *argv[])
- {
+int main(int argc, char *argv[])
+{
 	/* Unused varialble warning. */
 	(void)argv;
 	printf("%d\n", argc - 1);
 
 	return (0);
- }
+}
