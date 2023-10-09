d0dff7c9c39c04570a276246038a1b7f07d8a559
SandileG <sandilegraham@gmail.com>
2023-10-09 09:08:00 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
  or exit with status 98 on failure.
  */
 
- void *malloc_checked(unsigned int b)
- {
+void *malloc_checked(unsigned int b)
+{
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
 
 	return (ptr);
- }
+}
