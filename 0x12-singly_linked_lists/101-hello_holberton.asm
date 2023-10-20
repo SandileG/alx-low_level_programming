a2b3d027eb69c769eba30721d3dfbaf17c8f345a
SandileG <sandilegraham@gmail.com>
2023-10-20 02:13:30 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,27 +2,23 @@ section .data
 	hello db 'Hello, Holberton', 0
 	len equ $-hello
 	newline db 10
-	newline_len equ 1
 	format db '%s', 0
 
 section .text
 	extern printf
-
-global main
+	global main
 
 main:
-	sub	rsp, 8
-	mov	rsi, hello
-	mov	rdi, format
-	xor	rax, rax
-	call	printf
+	sub rsp, 8
+	mov rsi, hello
+	mov rdi, format
+	xor rax, rax
+	call printf
 
-	; Add a new line character
-	mov	rsi, newline    ; Load newline character
-	mov	rdx, newline_len
-	mov	rdi, format
-	xor	rax, rax
-	call	printf
+	mov rsi, newline    ; Add the new line character
+	mov rdi, format
+	call printf
 
-	add	rsp, 8
+	add rsp, 8
 	ret
+
