0939c04d26f4eac8f5fa46648d6a3f53bc24f5b1
SandileG <sandilegraham@gmail.com>
2023-10-21 15:28:35 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,21 @@
 #include "lists.h"
 
-void premain(void)
+/**
+ * print_before_main - Prints specific lines before main function execution.
+ */
+void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
-int main(void)
+/**
+ * first_main - check the code.
+ *
+ * Return: Always 0.
+ */
+int first_main(void)
 {
-	premain();
-	printf("(A tortoise, having pretty good sense of a hare's nature,
-		challenges one to a race.)\n");
+	print_before_main();
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 	return (0);
 }
