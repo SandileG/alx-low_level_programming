291166d5effdd6233a93a0240bc9bba9656190a7
SandileG <sandilegraham@gmail.com>
2023-10-21 14:54:21 -0700

    lists.h

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 
-/**
- * premain - Prints a specific message before the main function is executed.
- */
 void premain(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 		"I bore my house upon my back!\n");
+}
+
+int main(void)
+{
+	premain();
+	printf("(A tortoise, having pretty good sense of a hare's nature,
+		challenges one to a race.)\n");
+	return (0);
 }
