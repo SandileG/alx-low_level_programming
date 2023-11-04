0740a5094e8d061da4634bb97c26578f797f2258
SandileG <sandilegraham@gmail.com>
2023-11-04 12:52:38 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	}
 
 	source_fd = open(argv[1], O_RDONLY);
-	
+
 	if (source_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 	}
 
 	destination_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-                                        S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+					S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (destination_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	}
 
 	copy_file(source_fd, destination_fd);
-	
+
 	if (close(source_fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
