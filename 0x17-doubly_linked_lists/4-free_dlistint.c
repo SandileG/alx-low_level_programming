7f725de3377a0b6d2d9f31b26c172638c3e84a63
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-12 13:04:21 +0200

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,13 @@
 #include "lists.h"
 
 /**
- * free_dlistint Frees a dlistint_t list.
+ * free_dlistint - Frees a dlistint_t linked list.
  * @head: Pointer to the head of the doubly linked list.
+ *
+ * Description: This function frees a doubly linked list of type dlistint_t.
+ * It iterates through the list, freeing each node and updating the head
+ * pointer until the entire list is freed.
  */
-
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
