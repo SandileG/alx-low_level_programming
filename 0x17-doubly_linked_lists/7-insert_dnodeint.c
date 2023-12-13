e4c5404498f77ff7e720c090f95ffef900bc3436
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-13 20:33:54 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,53 +1,91 @@
 #include "lists.h"
 
+#define COUNT_INIT 0
+
+/**
+ * insert_dnodeint_at_head - Inserts a new node at the head of the list.
+ * @h: Pointer to the head of the list.
+ * @n: Value to be stored in the new node.
+ *
+ * Return: Pointer to the new node, or NULL on failure.
+ */
+static dlistint_t *insert_dnodeint_at_head(dlistint_t **h, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *h;
+	if (*h)
+	{
+		(*h)->prev = new_node;
+	}
+	*h = new_node;
+
+	return (new_node);
+}
+
+/**
+ * traverse_to_index - Traverses the list to a given index.
+ * @h: Head of the list.
+ * @idx: Target index.
+ *
+ * Return: Pointer to the node at the given index, or NULL if out of bounds.
+ */
+static dlistint_t *traverse_to_index(dlistint_t *h, unsigned int idx)
+{
+	unsigned int count = COUNT_INIT;
+	while (h && count < idx)
+	{
+		h = h->next;
+		count++;
+	}
+    return ((count == idx) ? h : NULL);
+}
+
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: Pointer to the address of the head of the doubly linked list.
- * @idx: Index where the new node should be added (starting from 0).
+ * @h: Pointer to the address of the head of the list.
+ * @idx: Index where the new node should be added. Index starts at 0.
  * @n: Value to be stored in the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp = *h;
-	unsigned int count = 0;
-
-	if (!h || (idx > 0 && !temp))
+	if (!h || idx > 0 && !*h)
+	{
 		return (NULL);
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
+	}
 
 	if (!idx)
 	{
-		if (!temp || ((*h)->prev == new_node))
-		{
-			new_node->next = *h;
-			*h = new_node;
-			return (*h);
-		}
+		return (insert_dnodeint_at_head(h, n));
 	}
 
-	while (temp && count < idx)
-		temp = temp->next, count++;
-
-	if (count < idx)
+	dlistint_t *prev = traverse_to_index(*h, idx - 1);
+	if (!prev)
 	{
-		free(new_node);
 		return (NULL);
 	}
 
-	new_node->prev = temp->prev;
-	new_node->next = temp;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
 
-	if (temp->prev)
-		temp->prev->next = new_node;
-	else
-		*h = new_node;
-	temp->prev = new_node;
+	new_node->n = n;
+	new_node->next = prev->next;
+	new_node->prev = prev;
+	if (prev->next)
+	{
+		prev->next->prev = new_node;
+	}
+	prev->next = new_node;
+
 	return (new_node);
 }
